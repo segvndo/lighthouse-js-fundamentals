5acf6b8e2a89a0014bee1fc6e35db400ad60a044
Bea Mungcal <beamungcal@hotmail.com>
2023-02-19 16:50:20 -0500

    Created ingredients file that lists ingredients with for loop and while loop, as well as in reverse order

---
+++ ingredients.js
@@ -0,0 +1,11 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+let i = 0
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+console.log(ingredients.reverse(i));
